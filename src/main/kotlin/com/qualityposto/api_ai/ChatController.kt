3982c34a8c575ec92ffedb434ffd031adf8e284f
Lucas Anjos <lucas.batista@qualityautomacao.com>
2026-01-23 16:47:13 -0300

    commit chat memory

--- src/main/kotlin/com/qualityposto/api_ai/ChatController.kt
+++ src/main/kotlin/com/qualityposto/api_ai/ChatController.kt
@@ -2,8 +2,8 @@ package com.qualityposto.api_ai
 
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
 import org.springframework.ai.chat.memory.ChatMemory
-import org.springframework.ai.chat.memory.MessageWindowChatMemory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -13,24 +13,37 @@ import org.springframework.web.multipart.MultipartFile
 
 @RestController
 class ChatController (
-    chatClientBuilder: ChatClient.Builder
+    chatClientBuilder: ChatClient.Builder,
+    chatMemory: ChatMemory
 ) {
-    private val chatClient = chatClientBuilder.build()
+    private val chatClient =
+        chatClientBuilder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build()).build()
 
     @GetMapping("/ai")
-    fun generation(userInput: String): String? {
+    fun conversaOpenAi(userInput: String): String? {
         return chatClient.prompt()
             .user(userInput)
             .call()
             .content()
     }
 
-    @PostMapping("/excel")
+    @GetMapping("/ai/memory")
+    fun conversaOpenAiMemory(
+        @RequestParam userInput: String,
+        @RequestParam chatId: String
+    ): String? {
+        return chatClient.prompt()
+            .user(userInput)
+            .advisors { it.param(ChatMemory.CONVERSATION_ID, chatId) }
+            .call()
+            .content()
+    }
+
+    @PostMapping("/excel/analise")
     fun analyzeExcel(
         @RequestParam("arquivo") arquivo: MultipartFile,
         @RequestParam("instrucao") instrucao: String
     ): String? {
-
         val excelData = StringBuilder()
 
         arquivo.inputStream.use { inputStream ->
@@ -50,11 +63,46 @@ class ChatController (
             .user { u ->
                 u.text("""
                 Instrução do Usuário: ${instrucao}
-                
+
                 Dados do Excel:
                 ${excelData}
             """.trimIndent())
             }
+            .call()
+            .content()
+    }
+
+    @PostMapping("/excel/analise/memory")
+    fun conversaOpenAiMemoryAnaliseExcel(
+        @RequestParam("arquivo") arquivo: MultipartFile,
+        @RequestParam("instrucao") instrucao: String,
+        @RequestParam("chatId") chatId: String
+    ): String? {
+        val excelData = StringBuilder()
+
+        arquivo.inputStream.use { inputStream ->
+            val workbook = WorkbookFactory.create(inputStream)
+            val sheet = workbook.getSheetAt(0)
+
+            for (row in sheet) {
+                for (cell in row) {
+                    excelData.append(cell.toString()).append(" | ")
+                }
+                excelData.append("\n")
+            }
+            workbook.close()
+        }
+
+        return chatClient.prompt()
+            .user { u ->
+                u.text("""
+                Instrução do Usuário: ${instrucao}
+
+                Dados do Excel:
+                ${excelData}
+            """.trimIndent())
+            }
+            .advisors { it.param(ChatMemory.CONVERSATION_ID, chatId) }
             .call()
             .content()
     }
